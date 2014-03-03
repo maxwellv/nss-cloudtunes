48468c25ed18874f3835c350c13dce30918291c9
Peder Schacht <peteschacht@gmail.com>
2014-03-03 03:02:10 -0600

    jade files including accordion should be functional once the routes with defaults are finalized

--- app/static/js/source/main.js
+++ app/static/js/source/main.js
@@ -6,6 +6,21 @@
 
   function initialize(){
     $(document).foundation();
+    $('#newArtist').click(clickNewArtist);
+    $('#newAlbum').click(clickNewAlbum);
+    $('#newSong').click(clickNewSong);
+  }
+
+  function clickNewArtist(){
+    $('#newArtistForm').toggleClass('hide');
+  }
+
+  function clickNewAlbum(){
+    $('#newAlbumForm').toggleClass('hide');
+  }
+
+  function clickNewSong(){
+    $('#newSongForm').toggleClass('hide');
   }
 
 })();
